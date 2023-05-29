661ac426ad400fcc4b242e527e9a5696a2f40959
NithinPathikonda <nithin.mahesh.pathikonda@gmail.com>
2023-05-29 22:15:43 +0530

    commit the code

--- server.py
+++ server.py
@@ -1,19 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from dataclasses import dataclass
 
-app = Flask(__name__)
+
+@dataclass
+class Result:
+    result: int
+
+
+app = Flask(_name_)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
+
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    first = data.get('first')
+    second = data.get('second')
+
+    if first is None or second is None:
+        return jsonify(error='Missing parameters'), 400
+
+    result = first + second
+    return jsonify(result=result)
+
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    first = data.get('first')
+    second = data.get('second')
 
-if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    if first is None or second is None:
+        return jsonify(error='Missing parameters'), 400
+
+    result = first - second
+    return jsonify(result=result)
+
+
+if _name_ == '_main_':
+    app.run(port=8080, host='0.0.0.0')
